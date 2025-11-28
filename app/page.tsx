2d492518c5f2dde4782f8617e0cde3b293ef60f5
Sabio <gamester.sad@gmail.com>
2025-11-28 23:40:51 +0000

    bump flavourtown version to 0.477

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-flow-col gap-1 p-8 h-full">
       <div className="grow-0 shrink basis-auto">
         <h1 className="text-5xl">Pan API</h1>
-        <p>v(a)0.01+v(c)0.417</p>
+        <p>v(a)0.01+v(c)0.477</p>
       </div>
       <div className="flex flex-col gap-3 justify-center items-center grow shrink basis-auto">
         <Image src="/deno-dino.jpg" alt="" width={100} height={100}/>
